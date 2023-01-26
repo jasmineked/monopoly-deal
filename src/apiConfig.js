195b9ab08b312f8c8388a04cc305d60ee3c01d27
jasmineked <jked630@icloud.com>
2023-01-26 12:58:52 -0500

    first deploy

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://git.heroku.com/pacific-ocean-94674.git',
   development: 'http://localhost:4741'
 }
 
